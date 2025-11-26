7603ec8eea5e590f0096eed40ee4b7c5dbd0d016
ncubeg <ncubeg@clarkson.edu>
2025-11-26 18:22:55 -0500

    Corrected LoginActivity TODOs for Task 5

--- app/src/main/java/clarkson/ee408/tictactoev4/LoginActivity.java
+++ app/src/main/java/clarkson/ee408/tictactoev4/LoginActivity.java
@@ -7,13 +7,19 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
-import clarkson.ee408.tictactoev4.model.User;
+import com.google.gson.GsonBuilder;
 
 import android.content.Intent;
 import android.widget.Toast;
-import com.google.gson.GsonBuilder;
-import clarkson.ee408.tictactoev4.socket.PairingResponse;
-import clarkson.ee408.tictactoev4.socket.SocketClient;
+
+import clarkson.ee408.tictactoev4.model.User;
+import clarkson.ee408.tictactoev4.client.SocketClient;
+import clarkson.ee408.tictactoev4.executor.AppExecutors;
+import clarkson.ee408.tictactoev4.network.Request;
+import clarkson.ee408.tictactoev4.network.RequestType;
+import clarkson.ee408.tictactoev4.network.Response;
+import clarkson.ee408.tictactoev4.network.ResponseStatus;
+
 import clarkson.ee408.tictactoev4.PairingActivity;
 import clarkson.ee408.tictactoev4.RegisterActivity;
 
@@ -50,35 +56,48 @@ public class LoginActivity extends AppCompatActivity {
         String password = passwordField.getText().toString();
 
         // Verify that all fields are not empty before proceeding. Toast with the error message
-        if (username.isEmpty() || password.isEmpty()) {
+        if (username.isBlank() || password.isBlank()) {
             Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
             return;
         }
 
-        // Create User object with username and password and call submitLogin()
-        User user = new User(username, password);
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
         submitLogin(user);
     }
+
     /**
      * Sends a LOGIN request to the server
      * @param user User object to login
      */
     public void submitLogin(User user) {
         // Send a LOGIN request, If SUCCESS response, call gotoPairing(), else, Toast the error message from sever
-        String jsonRequest = gson.toJson(user);
+        String serializedUser = gson.toJson(user);
+        Request request = new Request(RequestType.LOGIN, serializedUser);
 
-        // Send LOGIN request through the socket client (provided in project)
-        SocketClient.getInstance().sendRequest("LOGIN", jsonRequest, responseJson -> {
-            // Parse server response
-            PairingResponse response = gson.fromJson(responseJson, PairingResponse.class);
+        AppExecutors.getInstance().networkIO().execute(() -> {
+            try {
+                Response response = SocketClient.getInstance().sendRequest(request, Response.class);
 
-            runOnUiThread(() -> {
-                if (response.getStatus().equals("SUCCESS")) {
-                    gotoPairing(user.getUsername());
-                } else {
-                    Toast.makeText(this, response.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            });
+                AppExecutors.getInstance().mainThread().execute(() -> {
+                    if (response != null) {
+                        if (response.getStatus() == ResponseStatus.SUCCESS) {
+                            gotoPairing(user.getUsername());
+                        } else {
+                            Toast.makeText(this, response.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        Toast.makeText(this, "Error parsing response", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            } catch (Exception e) {
+                AppExecutors.getInstance().mainThread().execute(() ->
+                        Toast.makeText(this, "Error sending request", Toast.LENGTH_SHORT).show()
+                );
+            }
         });
     }
 
@@ -87,7 +106,6 @@ public class LoginActivity extends AppCompatActivity {
      * @param username the data to send
      */
     public void gotoPairing(String username) {
-        // Start PairingActivity and pass the username
         Intent intent = new Intent(this, PairingActivity.class);
         intent.putExtra("username", username);
         startActivity(intent);
@@ -97,7 +115,6 @@ public class LoginActivity extends AppCompatActivity {
      * Switch the page to {@link RegisterActivity}
      */
     public void gotoRegister() {
-        //Start RegisterActivity
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
     }
