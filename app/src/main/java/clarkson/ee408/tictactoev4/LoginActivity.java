ab0a678560d89ea5a0b03db4f2600e883544ddfe
ncubeg <ncubeg@clarkson.edu>
2025-11-26 10:24:55 -0500

    Completed LoginActivity TODOs for Task 5

--- app/src/main/java/clarkson/ee408/tictactoev4/LoginActivity.java
+++ app/src/main/java/clarkson/ee408/tictactoev4/LoginActivity.java
@@ -9,6 +9,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.gson.Gson;
 import clarkson.ee408.tictactoev4.model.User;
 
+import android.content.Intent;
+import android.widget.Toast;
+import com.google.gson.GsonBuilder;
+import clarkson.ee408.tictactoev4.socket.PairingResponse;
+import clarkson.ee408.tictactoev4.socket.SocketClient;
+import clarkson.ee408.tictactoev4.PairingActivity;
+import clarkson.ee408.tictactoev4.RegisterActivity;
+
 public class LoginActivity extends AppCompatActivity {
 
     private EditText usernameField;
@@ -26,7 +34,8 @@ public class LoginActivity extends AppCompatActivity {
         usernameField = findViewById(R.id.editTextUsername);
         passwordField = findViewById(R.id.editTextPassword);
 
-        // TODO: Initialize Gson with null serialization option
+        // Initialize Gson with null serialization option
+        gson = new GsonBuilder().serializeNulls().create();
 
         //Adding Handlers
         loginButton.setOnClickListener(view -> handleLogin());
@@ -40,17 +49,37 @@ public class LoginActivity extends AppCompatActivity {
         String username = usernameField.getText().toString();
         String password = passwordField.getText().toString();
 
-        // TODO: verify that all fields are not empty before proceeding. Toast with the error message
+        // Verify that all fields are not empty before proceeding. Toast with the error message
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
-        // TODO: Create User object with username and password and call submitLogin()
+        // Create User object with username and password and call submitLogin()
+        User user = new User(username, password);
+        submitLogin(user);
     }
-
     /**
      * Sends a LOGIN request to the server
      * @param user User object to login
      */
     public void submitLogin(User user) {
-        // TODO: Send a LOGIN request, If SUCCESS response, call gotoPairing(), else, Toast the error message from sever
+        // Send a LOGIN request, If SUCCESS response, call gotoPairing(), else, Toast the error message from sever
+        String jsonRequest = gson.toJson(user);
+
+        // Send LOGIN request through the socket client (provided in project)
+        SocketClient.getInstance().sendRequest("LOGIN", jsonRequest, responseJson -> {
+            // Parse server response
+            PairingResponse response = gson.fromJson(responseJson, PairingResponse.class);
+
+            runOnUiThread(() -> {
+                if (response.getStatus().equals("SUCCESS")) {
+                    gotoPairing(user.getUsername());
+                } else {
+                    Toast.makeText(this, response.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        });
     }
 
     /**
@@ -58,13 +87,18 @@ public class LoginActivity extends AppCompatActivity {
      * @param username the data to send
      */
     public void gotoPairing(String username) {
-        // TODO: start PairingActivity and pass the username
+        // Start PairingActivity and pass the username
+        Intent intent = new Intent(this, PairingActivity.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
     }
 
     /**
      * Switch the page to {@link RegisterActivity}
      */
     public void gotoRegister() {
-        // TODO: start RegisterActivity
+        //Start RegisterActivity
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
