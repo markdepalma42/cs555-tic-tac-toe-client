05a17c6cf6a1d7702c47a515adfadabcdf5b8b6e
ncubeg <ncubeg@clarkson.edu>
2025-11-27 09:44:17 -0500

    Corrected LoginActivity TODOs for Task 5

--- app/src/main/java/clarkson/ee408/tictactoev4/LoginActivity.java
+++ app/src/main/java/clarkson/ee408/tictactoev4/LoginActivity.java
@@ -3,7 +3,6 @@ package clarkson.ee408.tictactoev4;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.Gson;
@@ -12,16 +11,13 @@ import com.google.gson.GsonBuilder;
 import android.content.Intent;
 import android.widget.Toast;
 
-import clarkson.ee408.tictactoev4.model.User;
+import clarkson.ee408.tictactoev4.client.AppExecutors;
 import clarkson.ee408.tictactoev4.client.SocketClient;
-import clarkson.ee408.tictactoev4.executor.AppExecutors;
-import clarkson.ee408.tictactoev4.network.Request;
-import clarkson.ee408.tictactoev4.network.RequestType;
-import clarkson.ee408.tictactoev4.network.Response;
-import clarkson.ee408.tictactoev4.network.ResponseStatus;
-
-import clarkson.ee408.tictactoev4.PairingActivity;
-import clarkson.ee408.tictactoev4.RegisterActivity;
+import clarkson.ee408.tictactoev4.model.User;
+import clarkson.ee408.tictactoev4.socket.Request;
+import clarkson.ee408.tictactoev4.socket.RequestType;
+import clarkson.ee408.tictactoev4.socket.Response;
+import clarkson.ee408.tictactoev4.socket.ResponseStatus;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -60,11 +56,9 @@ public class LoginActivity extends AppCompatActivity {
             Toast.makeText(this, "Please enter both username and password", Toast.LENGTH_SHORT).show();
             return;
         }
-
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
-
         submitLogin(user);
     }
 
@@ -80,7 +74,6 @@ public class LoginActivity extends AppCompatActivity {
         AppExecutors.getInstance().networkIO().execute(() -> {
             try {
                 Response response = SocketClient.getInstance().sendRequest(request, Response.class);
-
                 AppExecutors.getInstance().mainThread().execute(() -> {
                     if (response != null) {
                         if (response.getStatus() == ResponseStatus.SUCCESS) {
@@ -92,7 +85,6 @@ public class LoginActivity extends AppCompatActivity {
                         Toast.makeText(this, "Error parsing response", Toast.LENGTH_SHORT).show();
                     }
                 });
-
             } catch (Exception e) {
                 AppExecutors.getInstance().mainThread().execute(() ->
                         Toast.makeText(this, "Error sending request", Toast.LENGTH_SHORT).show()
